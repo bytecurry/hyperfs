06ae037efea9aee461cb8325d3936231df3c3139
Thayne McCombs <astrothayne@gmail.com>
2015-09-20 19:20:51 -0600

    Allow specifying port as argument

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ it is run from. It is primarily intended as a development tool similar to using
 `python -m SimpleHTTPServer`. But it is a single lightweight executable written in rust.
 
 To be honest, part of the motivation was to just so I could try out rust.
+
+## Usage
+
+To use simple cd into the directory with the files you want to server and run
+
+```bash
+$ hyperfs >port<
+```
+
+where `>port<` is the port you want to listen on. If not supplied it will listen on port
+3000.
+
+## Installation
+
+You can either download the binary from the latest release (currently only 64-bit linux version
+is available). Or you can build from source using
+
+```bash
+$ cargo build --release
+```
+and the resulting binary will be at 'target/release/hyperfs'.
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::io;
 use std::fs::File;
 use std::path::Path;
 
+use std::net::{Ipv4Addr, SocketAddrV4};
+
 use hyper::Server;
 use hyper::server::Request;
 use hyper::server::Response;
@@ -35,9 +37,14 @@ fn extract_path(uri: &String) -> &Path {
 }
 
 fn main() {
-    match Server::http("127.0.0.1:3000") {
+    let ip = Ipv4Addr::new(127, 0, 0, 1);
+    let port = std::env::args().nth(1).and_then(|p| p.parse::<u16>().ok()).unwrap_or(3000);
+
+    let addr = SocketAddrV4::new(ip, port);
+
+    match Server::http(addr) {
         Ok(server) => {
-            println!("Server is listening at http://127.0.0.1:3000");
+            println!("Server is listening at http://{}", addr);
             server.handle(handle_request).unwrap();
         },
         Err(error) => {
